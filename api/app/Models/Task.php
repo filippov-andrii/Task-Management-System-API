2fcc0be2a8105b56bfc461414cd2e096e2d58fad
Filippov Andrii <saintrius@gmail.com>
2025-11-24 22:59:20 +0100

    Added Project model, updated Task model with deadline and relations

--- api/app/Models/Task.php
+++ api/app/Models/Task.php
@@ -7,12 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory;
 
     protected $fillable = [
         'title',
         'description',
         'status',
+        'deadline',
+        'user_id',
+        'project_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
